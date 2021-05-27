e5c0b7c22fe483d489b3064635ab6efd8e2c60d6
KashmiraBhake <kashmirabhake@gmail.com>
2021-05-27 17:39:53 +0000

    working code

---
+++ src/main/java/com/example/demo/PatientDetails.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@EnableAutoConfiguration
+public class PatientDetails {
+    @RequestMapping("/")
+    public ModelAndView home()
+    {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("patientform");
+        return modelAndView;
+    }
+}
